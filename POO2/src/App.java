0d92cce7840119a669f5a6b25c1e258a80a0e177
Kalil <106711007+Kal1l@users.noreply.github.com>
2022-06-19 07:50:14 -0300

    Add files via upload

--- POO2/src/App.java
+++ POO2/src/App.java
@@ -1,10 +1,8 @@
 import java.io.File;
 import java.util.Scanner;
+import java.util.ArrayList;
 /*Lista de afazeres:
     -Fazer menu
-
-    -Fazer uma forma de editar um arquivo ja criado
-    (mecanismo de pesquisa de disciplina[?])
 
     -Fazer o código rodar para mais de um único aluno
 
@@ -12,30 +10,55 @@ import java.util.Scanner;
 
     -Encontrar alguma forma de transformar os dados do arquivo,
     em um array de objetos(ou de respostas/notas)
-    e transformar esse array em um objeto novamente*/
+    -ler a linha do arquivo e adiconar num array;
+    -separar a linha com um .split() separando em respostas e nome do aluno
+    e transformar esse array em um objeto novamente
+    
+    -Usar .split pra separar o nome das repostas*/
+
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
         Disciplina disciplina=new Disciplina();
-        Aluno aluno=new Aluno();
+        //Aluno aluno=new Aluno();
+        ArrayList<String> disciplinas = new ArrayList<String>();
+        //ArrayList<String> listaAlunos = new ArrayList<String>();
+        //ArrayList<String> listaRespostas = new ArrayList<String>();
         //cria a pasta das discipinas
         File dir=new File("Disciplinas");
         dir.mkdir();
-
-        //criando uma nova disciplina
-        System.out.println("Nome disciplina: ");
-        String nomeDisciplina=input.nextLine();
-        System.out.println("Respostas e Aluno ");
-        String respostas=input.nextLine();String nomeAluno= input.nextLine();
-        String dados = respostas.toUpperCase()+"\t"+nomeAluno;
-        disciplina.novoArquivo(dir,nomeDisciplina,dados);
-
-        //calcular a nota do aluno
+        int opção=0;
+        while(opção!=3){
+        System.out.println("ESCOLHA UMA OPÇÃO:\n1-CRIAR DISCIPLINA\n2-EDITAR DISCIPLINA\n3-SAIR");
+        opção=input.nextInt();
+            switch(opção){
+                case 1:
+                //criando uma nova disciplina
+                System.out.println("Nome disciplina: ");
+                String nomeDisciplina= input.next();
+                System.out.println("Respostas e Aluno ");
+                String respostas=input.nextLine();
+                String nomeAluno= input.nextLine();
+                String dados = respostas.toUpperCase()+"\t"+nomeAluno;
+                disciplina.novoArquivo(dir,nomeDisciplina,dados);
+                disciplina.novaDisciplina(dir,nomeDisciplina);
+                continue;
+                case 2:
+                System.out.println("Insira o nome da disciplina que deseja editar:");
+                String nomeDisciplinaProcurada=input.next();
+                for(String elemento : disciplinas){
+                    if(elemento.equals(nomeDisciplinaProcurada)){
+                        System.out.println(elemento);
+                    }
+                }
+                
+    
+            }
+        }
+        /*calcular a nota do aluno
         System.out.println("Digite o gabarito das questões");
         String gabarito=input.nextLine();
-        System.out.println(aluno.calcularNota(respostas, gabarito.toUpperCase()));
+        System.out.println(aluno.calcularNota(respostas, gabarito.toUpperCase()));*/
         input.close();
-    }
+            }
 }
-
-
--- POO2/src/Disciplina.java
+++ POO2/src/Disciplina.java
@@ -1,6 +1,8 @@
 import java.io.*;
-
+import java.util.ArrayList;
 public class Disciplina {
+
+    //cria uma disciplina(pasta e arquivo com o mesmo nome da disciplina)
     public void novoArquivo(File dir,String disciplina,String dados){
         File dirFile=new File(dir,disciplina);
         dirFile.mkdir();
@@ -27,7 +29,29 @@ public class Disciplina {
         }
     }catch(IOException e){}       
     }
-
+    public void novaDisciplina(File dir,String disciplina){
+        File file = new File(dir,"disciplinas.txt");
+        try{file.createNewFile();}catch(IOException e){}
+        try{
+        FileReader ler = new FileReader(file);
+        FileWriter fileWriter = new FileWriter(file,true);     
+        BufferedWriter escrever = new BufferedWriter(fileWriter); 
+        
+        escrever.write (disciplina);
+        escrever.newLine();
+        escrever.close();
+        fileWriter.close();      
+        BufferedReader lerb = new BufferedReader(ler);
+        String linha = lerb.readLine();
+        
+        lerb.close();
+        while(linha!= null){
+            System.out.println(linha);
+            linha = lerb.readLine();
+        }
+    }catch(IOException e){}       
+    }
+    //cria o gabarito de uma disciplina
     public void gabarito(File dir,String gabarito){
         File file=new File(dir,"gabarito.txt");
         try{
@@ -48,4 +72,26 @@ public class Disciplina {
             }
         }catch(IOException e){}
     }
+
+    public String procurarDisciplina(String procura,File disciplinas){
+    ArrayList<String> linha = new ArrayList<>();
+    try {
+    FileReader reader=new FileReader(disciplinas);
+    BufferedReader bReader = new BufferedReader(reader);
+    String ler;
+    bReader.close();
+    while (( ler = bReader.readLine()) != null) {
+    linha.add(ler);
+    }
+    } catch (IOException e) {}
+    String[] lista = linha.toArray(new String[0]);
+    
+        for(String elemento : lista){
+            if(elemento.equals(procura)){
+                return elemento;
+            }
+        }
+    }
+
+
 }
